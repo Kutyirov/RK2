90c398aa7a7c729d6cb784b9462ef2d0265251c1
Kutyirov <37041655+Kutyirov@users.noreply.github.com>
2018-04-26 23:40:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,41 @@
-#include <matrix.hpp>
+#include <queue.hpp>
 
 int main() {
-  Matrix m1{3,5};
-  Matrix m2{1, 2, 3, 4, 5, 6};
-  Matrix m3{m2};
-  
-  cout << m1 << endl; 
-  cout << m2 << endl;
-  cout << m3 << endl;
- 
-  Matrix m4;
-  m4 = m3;
-  cout << m4 << endl;
- 
-  m4[0][0] = 8;
-  cout << m4 << endl;
- 
-  cout << boolalpha 
-    << (m2 == m3) 
-    << (m3 == m4) << endl; 
-      
-  cout << m1.rows()      
-    << m1.columns() << endl; 
- 
-  Matrix m5{0, 2, 4, 6, 8, 10};    
-  cout << m3 + m5 << endl; 
-  cout << m3 - m5 << endl;
 
-  Matrix m6{}; 
-  cout << m6.empty();
-
-  ofstream out{"matrix.bin"};
-  out << m3;
+  Queue <int> q1;
   
-  ifstream in{"matrix.bin"};
-  in >> m6;
-  m3.swap(m6);
-  cout << m3 << endl;
+  Queue <int> q2 = {1, 2, 3, 4};
+
+  Queue <int> q3{q2};
+
+  cout << q2 << endl;
+  cout << q3 << endl;
+
+  q1.swap(q3);
+  cout << q1 << endl;
+
+  cout << boolalpha
+       << q3.empty();
+ 
+  cout << q1.size() << endl;
+
+  q3.push(5);
+  cout << q3 << endl;
+   
+  ofstream output{ "queue.bin" };
+  output << q3;
+  
+  Queue <int> q4;
+  ifstream input{ "queue.bin" };
+  input >> q4;
+  
+  cout << (q4 == q3) << endl; 
+
+  cout << q4.front() << endl;
+  cout << q4.back() << endl;
+  
+  q4.pop();
+  cout << q4 << endl;
+
+  q4.front() = 6;
 }
